aa1e2b5bebaf25017d4725ef864257a6562dee0e
katanasushi <83367992+katanasushi@users.noreply.github.com>
2022-07-25 20:39:54 +0200

    Update caliper_setup.sh
    Signed-off-by: katanasushi <83367992+katanasushi@users.noreply.github.com>

--- caliper_setup.sh
+++ caliper_setup.sh
@@ -4,7 +4,7 @@ set -x
 install_and_bind () {
 	npm init -y
 	npm install --save @hyperledger/caliper-cli@0.4.0
-	sed -i '' "s/this.checkConfig(ethereumConfig);/\/\/this.checkConfig(ethereumConfig);/g" node_modules/@hyperledger/caliper-ethereum/lib/ethereum-connector.js
+	sed -i "s/this.checkConfig(ethereumConfig);/\/\/this.checkConfig(ethereumConfig);/g" node_modules/@hyperledger/caliper-ethereum/lib/ethereum-connector.js
 	npx caliper bind --caliper-bind-sut ethereum:1.2.1
 }
 
